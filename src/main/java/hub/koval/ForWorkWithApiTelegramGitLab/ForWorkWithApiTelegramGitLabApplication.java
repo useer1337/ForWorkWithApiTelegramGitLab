0ef02ee46b2b5e62eff141309619b32a3d6ce9f2
useer1337 <koval.alexander00@gmail.com>
2020-07-07 15:08:55 +0500

    Инициализировать проект

---
+++ src/main/java/hub/koval/ForWorkWithApiTelegramGitLab/ForWorkWithApiTelegramGitLabApplication.java
@@ -0,0 +1,13 @@
+package hub.koval.ForWorkWithApiTelegramGitLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForWorkWithApiTelegramGitLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForWorkWithApiTelegramGitLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hub/koval/ForWorkWithApiTelegramGitLab/ForWorkWithApiTelegramGitLabApplicationTests.java
@@ -0,0 +1,13 @@
+package hub.koval.ForWorkWithApiTelegramGitLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForWorkWithApiTelegramGitLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
